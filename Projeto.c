8ef90a6320a3c53c1ed82c34fb26e3e51d1fc825
MatheusMendes <111511128+matheusMenDs@users.noreply.github.com>
2022-10-31 16:05:23 -0300

    Update and rename Projeto Completo to Projeto.c

---
+++ Projeto.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <ctime>
+#include <time.h>
+
+int main(){
+	char nome[50][1000];
+	char endereco[50][1000];
+	char sexo[20][1000];
+	double altura[1000];
+	int i, loop = 1;
+	int vacina[1000];
+	
+	printf("==========|     |BEM VINDO|     |==========\n");
+	printf("==========| CADASTRO DE USUARIO |==========\n\n");
+	
+	srand(time(NULL));
+	
+	for(i=0; i<1000; i++){
+		printf("Digite o nome completo: ");
+		fgets(nome[i], 50, stdin);
+	
+		printf("Digite o endereco do usuario: ");
+        fgets(endereco[i], 50, stdin);
+	
+		printf("Digite o sexo do usuario:\n[M]asculino\t[F]eminino\t[ND]nao declarar:   ");
+        scanf("%s", &sexo[i]);
+        if(strcmp(sexo[i], "M") == 0 || (strcmp(sexo[i], "F") == 0) || (strcmp(sexo[i], "ND") == 0)) 
+        {
+            loop = 1;
+        }
+        else{
+            printf("Sexo %s Invalido\n", sexo[i]);
+            break;
+        }
+	
+		printf("Digite a altura do usuario: ");
+        scanf("%lf", &altura[i]);
+        if(altura[i] < 1 || altura[i] > 2){
+            printf("Altura invalida\n");
+        }
+	
+		printf("Se vacinou?\n[S]im\t[N]ao:   ");    //AJUSTE BOOLEAN
+		fflush(stdin);
+		scanf("%d", &vacina[i]);
+	
+		printf("ID do usuario: %d\n", rand() % 1000);
+	}
+}
